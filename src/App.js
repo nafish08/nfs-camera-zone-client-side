c9f216f1a4509ab3e60194540a81b4408d7392ba
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 16:45:48 +0600

    Home page created with navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="">
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
