6655b121db8bfc41453516d95ee328abe6cbf120
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 17:24:01 +0600

    Banner section added in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Shared/Header';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
         </div>
     );
 };
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -23,15 +23,10 @@ const Header = () => {
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">NFS Camera Zone</a>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     {menuItems}
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
             </div>
         </div>
     );
