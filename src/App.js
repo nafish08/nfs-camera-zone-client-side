a5e7634fe07ccc30f3a72525ed66f75f7a78c482
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 20:55:18 +0600

    requireauth created and added with purchase page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
 import Purchase from './Pages/Purchase/Purchase';
 import Footer from './Pages/Shared/Footer';
 import Header from './Pages/Shared/Header';
@@ -12,7 +14,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+        <Route path='/purchase' element={
+          <RequireAuth>
+            <Purchase></Purchase>
+          </RequireAuth>
+        }></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loading.css'
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center'>
+            <div class="loader">
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
