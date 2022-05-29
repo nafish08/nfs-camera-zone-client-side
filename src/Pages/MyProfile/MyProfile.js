eb6d1c2a9c6b3c5ab76be2aba4ab5f57690e2cb0
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 23:07:14 +0600

    My profile page created

--- src/Pages/MyProfile/MyProfile.js
+++ src/Pages/MyProfile/MyProfile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
     return (
         <div>
-            <h2>My Profile Page</h2>
+            <h2 className='text-3xl text-center font-bold my-12 uppercase'>Add or Update Your Info</h2>
+            <form >
+                <div class="form-control w-full max-w-xs mx-auto">
+                    <input type="text" name='name' value={user.displayName} class="input w-full max-w-xs" />
+                    <input type="email" name='email' value={user.email} class="input w-full max-w-xs mt-4" />
+                    <input type="text" name='edu' placeholder='Education Qualification' class="input w-full max-w-xs mt-4" />
+                    <input type="text" name='city' placeholder='Your City/District' class="input w-full max-w-xs mt-4" />
+                    <input type="text" name='phone' placeholder='Your Phone Number' class="input w-full max-w-xs mt-4" />
+                    <input type="text" name='linkedin' placeholder='Linkedin Profile Url' class="input w-full max-w-xs mt-4" />
+                    <input type="submit" value='Submit' class="input w-1/2 max-w-xs mt-4 mx-auto bg-primary text-white uppercase font-bold" />
+                    <input type="submit" value='Update' class="input w-1/2 max-w-xs mt-4 mx-auto bg-secondary text-white uppercase font-bold" />
+                </div>
+            </form>
         </div>
     );
 };
