ad03346e78f8205b43dd9b42b2b5aff3584bc1ea
nafish08 <md.nafish.sadik@gmail.com>
2022-05-30 23:09:12 +0600

    Purchase button functionality added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -7,7 +7,6 @@ const Dashboard = () => {
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet></Outlet>
-                <label for="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
             </div>
             <div className="drawer-side">
                 <label for="my-drawer-2" className="drawer-overlay"></label>
--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Part = ({ product }) => {
-    const { name, description, minquantity, quantity, price, img } = product;
+    const { _id, name, description, minquantity, quantity, price, img } = product;
+    const navigate = useNavigate();
+    const navigateToPurchsase = _id => {
+        // _id as parameter
+        navigate(`/part/${_id}`);
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -15,7 +20,7 @@ const Part = ({ product }) => {
                 <small>Available Quantity: {quantity}</small>
                 <small>Price: {price}BDT (per unit)</small>
                 <div className="card-actions">
-                    <Link to={'/purchase'}><button className="btn btn-primary text-white uppercase">Place Order</button></Link>
+                    <Link to={'/purchase'}><button onClick={() => navigateToPurchsase(_id)} className="btn btn-primary text-white uppercase">Place Order</button></Link>
                 </div>
             </div>
         </div>
